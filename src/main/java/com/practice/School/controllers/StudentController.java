b4cfe8a2e430ca7bc385de090d4c53917d3f285c
harshitjainhj <155512519+harshitjainhj@users.noreply.github.com>
2024-06-26 13:15:56 +0530

    added a customised exception handling

--- src/main/java/com/practice/School/controllers/StudentController.java
+++ src/main/java/com/practice/School/controllers/StudentController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.practice.School.entities.Student;
+import com.practice.School.exceptions.StudentNotFoundException;
 import com.practice.School.services.StudentService;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,46 +20,43 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @RestController
 @RequestMapping("api/students")
 public class StudentController {
 
-    @Autowired
-    private StudentService service;
+	@Autowired
+	private StudentService service;
 
-    @PostMapping
-    public  ResponseEntity<Student> createStudent(@RequestBody  Student student )
-    {
-    		Student createdStudent= 	service.createStudent(student);
-    		
-    		return ResponseEntity.ok(createdStudent);
-        
-    } 
-    @GetMapping 
-    public List<Student> getAllStudents(){
-    	
-    	
-    	return service.getAllStudents();
-    }
-    
-    @GetMapping ("/{id}")
-    public Optional<Student> getStudentById(@PathVariable int id )
-    {
-    	return service.getStudentById(id);
-    }
+	@PostMapping
+	public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+		Student createdStudent = service.createStudent(student);
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Student> updateStudent( @PathVariable int id, @RequestBody Student student) {
-    	Optional<Student> updatedStudent=service.updateStudent(id, student);
-    	
-    	return updatedStudent.map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound().build());
-    }
-    
-    @DeleteMapping("/{id}")
-    public void deleteStudent(@PathVariable int id) {
-    	 
-    	service.deleteStudent(id);
-    }
-    
+		return ResponseEntity.ok(createdStudent);
+
+	}
+	@GetMapping
+	public List<Student> getAllStudents() {
+		return service.getAllStudents();
+	}
+
+	@GetMapping("/{id}")
+	public Student getStudentById(@PathVariable int id) {
+		
+		
+		return service.getStudentById(id).orElseThrow(()-> new StudentNotFoundException("Student not found with the id: "+id));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Student> updateStudent(@PathVariable int id, @RequestBody Student student) {
+		Optional<Student> updatedStudent = service.updateStudent(id, student);
+
+		return updatedStudent.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteStudent(@PathVariable int id) {
+
+		service.deleteStudent(id);
+	}
+
 }
---
+++ src/main/java/com/practice/School/exceptions/StudentNotFoundException.java
@@ -0,0 +1,10 @@
+package com.practice.School.exceptions;
+
+public class StudentNotFoundException extends RuntimeException{
+	
+	public StudentNotFoundException(String message)
+	{
+		super(message);
+	}
+
+}
